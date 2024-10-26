74604d6710cfdc2274e2b9969f9550526fef4a8c
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-26 22:14:11 +0330

    add router and controller structure

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+
+const register = async (req,res,next)=>{
+
+
+}
+const login = async (req,res,next)=>{
+
+
+}
+const refresh = async (req,res,next)=>{
+
+
+}
+const me = async (req,res,next)=>{
+
+
+}
+const logout = async (req,res,next)=>{
+
+
+}
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const controller = require("./../controllers/auth")
 
 const router = express.Router();
 
-
+router.route("/register").post(controller.register);
+router.route("/login").post(controller.login);
+router.route("/refresh").post(controller.refresh);
+router.route("/me").post(controller.me);
+router.route("/logout").post(controller.logout);
 
 module.exports = router;
